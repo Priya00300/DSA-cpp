10fb75dd8ddd413e6226b0ff6e61caf8fdbc241c
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-25 20:56:15 +0000

    sorting

--- sorting/bublesort.cpp
+++ sorting/bublesort.cpp
@@ -21,14 +21,20 @@ using namespace std;
 void bubblesort(int arr[],int n){
   int temp;
   for(int i =n-1;i>=0;i--){
+    int didswap=0;
     
     for(int j=0;j<=i-1;j++){
       if(arr[j]>arr[j+1]){
         temp = arr[j+1];
           arr[j+1]=arr[j];
           arr[j]=temp;
+        didswap=1;
       }
     }
+    if(didswap==0){
+      break;
+    }  
+    cout<<"runs\n";
   
     
   }}
