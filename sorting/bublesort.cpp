10d0429457c7d9630936488bb70a495331a0fc00
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-25 20:50:45 +0000

    sorting

--- sorting/bublesort.cpp
+++ sorting/bublesort.cpp
@@ -16,3 +16,36 @@
 //9|13|20|24|46|52
 //done
 
+#include<iostream>  
+using namespace std;
+void bubblesort(int arr[],int n){
+  int temp;
+  for(int i =n-1;i>=0;i--){
+    
+    for(int j=0;j<=i-1;j++){
+      if(arr[j]>arr[j+1]){
+        temp = arr[j+1];
+          arr[j+1]=arr[j];
+          arr[j]=temp;
+      }
+    }
+  
+    
+  }}
+int main()
+{
+  int n;
+  cin >>n;
+  int arr[n];
+  for (int i =0; i<n;i++){
+    cin >> arr[i];
+  }
+  bubblesort(arr,n);
+  for(int i=0;i<n;i++){
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+
+
+}
