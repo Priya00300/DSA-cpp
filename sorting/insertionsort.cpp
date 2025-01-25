1b5efdde0379bfac63c2b2fc25fa7c53a45e81bb
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-25 21:15:28 +0000

    sorting

--- sorting/insertionsort.cpp
+++ sorting/insertionsort.cpp
@@ -15,3 +15,39 @@
 //13 is not at the correct place
 //9|6|8|12|13|14|15
 //done
+//algorithm-
+//we are runningg from 0 to n-1
+//fori=0;i<=n-1;i++)
+//for(j>0&&arr[j]<arr[j-1])
+
+#include<iostream>
+using namespace std;
+void insertionsort(int arr[],int n){
+  for(int i=0;i<n-1;i++){
+    int j=i;
+   while(j > 0 && arr[j-1]>arr[j]){
+      int temp = arr[j -1];
+      arr[j-1]=arr[j];
+      arr[j]=temp;
+     j--;
+    }
+  }
+}
+int main()
+  {
+    int n;
+    cin >>n;
+    int arr[n];
+    for (int i =0; i<n;i++){
+      cin >> arr[i];
+    }
+    insertionsort(arr,n);
+    for(int i=0;i<n;i++){
+      cout << arr[i] << " ";
+    }
+
+    return 0;
+
+
+  }
+
