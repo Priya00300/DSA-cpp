bd5e4195c7b7e110a02da1fea10270752da1d688
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-25 18:24:29 +0000

    new change

---
+++ sorting/bublesort.cpp
@@ -0,0 +1,9 @@
+//follows -adjacent swaping 
+//13|46|24|52|20|9
+//0 | 1| 2| 3| 4|5
+//n-6|n-5|n-4|n-3|n-2|n-1
+//1st step 13 and 46 will compare>13|46|24|52|20|9
+//2nd step 46 and 24 will >13|24|46|52|20|9
+//3rd step 46 and 52 will >13|24|46|52|20|9
+//4th step 46 and 20 will >13|24|46|20|52|9
+//5th step 46 and 9 will >13|24|46|20|9|52
