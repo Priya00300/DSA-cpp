2f95499cee8f10ce916e981f357a79beaceaeb65
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-26 20:34:59 +0000

    tress

--- trees/maxpathsum.cpp
+++ trees/maxpathsum.cpp
@@ -13,5 +13,32 @@ Node(int val) : data(val), left(nullptr), right(nullptr) {}
 }
 class solution {
 public:
-int maxpathsum
+int maxpathsum(Node* root){
+  int globalmax = INT_MIN;
+  int maxgain(Node* root,int &globalmax);
+  retrun globalmax;
+}
+private:
+int maxgain(Node* root,int &globalmax){
+  if(root==nullptr) return 0;
+
+  int leftmaxx = max(maxgain(root->left,globalmax));
+  int rightmaxx = max(maxgain(root->right,globalmax));
+  int currentpathsum = root->data + leftmaxx + rightmaxx;
+  globalmax = max(globalmax,currentpathsum);
+  return node->data +max(leftmaxx,rightmaxx);
+  
+}
+
+};
+int main(){
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->left = new Node(4);
+  root->left->right = new Node(5);
+  root->right->left = new Node(6);
+  root->right->right = new Node(7);
+  cout << "Maximum Path Sum: " << maxpathsum(root);
+  return 0;
 }
