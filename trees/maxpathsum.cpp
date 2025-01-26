4e552d7f3d9ece88539bf5dc11450bbb2a375ce4
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-26 20:28:36 +0000

    tress

---
+++ trees/maxpathsum.cpp
@@ -0,0 +1,2 @@
+//in this we will be using two functions first maxpathsum will be the main function inside of which we wil be calling globalmax and initialzing it to INT_MIN and then we will be updating it through a helper function called maxgain which will calculat which has the max sum for their values and then we will be updating the globalmax
+//code -
