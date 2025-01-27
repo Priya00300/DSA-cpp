c30469b02c7a366b5b044ad8ca8f5a4e3db83301
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-27 20:56:23 +0000

    array

--- Arrays/largestelement.cpp
+++ Arrays/largestelement.cpp
@@ -1,2 +1,27 @@
 //we can either sort it and then give out the last
 //or else we can iterate by assuming that first element is the largest
+
+#include<iostream>
+using namespace std;
+int largest(int arr[],int n){
+  int max=arr[0];
+  for(int i=0;i<n;i++){
+    if(arr[i]>max){
+      max=arr[i];
+    }
+  }
+  return max;
+}
+int main()
+{
+  int n,arr[n],i;
+  cout<<"enter the size of the array\n";
+  cin>>n;
+  cout<<"enter the elements of the array\n";
+  for(i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int max=largest(arr,n);
+  cout<<"the largest element is "<<max;
+  return 0;
+}
