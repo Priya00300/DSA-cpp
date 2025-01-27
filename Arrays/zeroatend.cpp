2e68bc5e83feb6508e98ffafb379285339a3f2e0
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-27 21:52:41 +0000

    array

---
+++ Arrays/zeroatend.cpp
@@ -0,0 +1,5 @@
+//collecting all the non zeros that will go at the end
+//and put them in temp
+//then all the remaining places will be zero
+//first iterating then temp then zeros
+//temp.add(arr[i]); if (arr[i]!=0) temp.add(arr[i]);
