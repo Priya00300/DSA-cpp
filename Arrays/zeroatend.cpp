0c8b7f03feef659e8802e670cad75e80d6d0ea47
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-27 21:56:29 +0000

    array

--- Arrays/zeroatend.cpp
+++ Arrays/zeroatend.cpp
@@ -3,3 +3,10 @@
 //then all the remaining places will be zero
 //first iterating then temp then zeros
 //temp.add(arr[i]); if (arr[i]!=0) temp.add(arr[i]);
+//inner loop fopr non zero for(i =nonzero;i<n;i
+//++){
+//arr[i]=0;
+//
+//O(N)
+//in the inner loop we are starting uit fromwhere the non zero is finised
+//2nd method we can check if it is 0 or not and if it is zero then we will swap it to the next one
