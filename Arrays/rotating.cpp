257bcc04027c21334a445fcfd7c5a3e6d40bf0ba
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-27 21:00:56 +0000

    array

---
+++ Arrays/rotating.cpp
@@ -0,0 +1,8 @@
+//we have to solv the problem in the givne array only
+//lets see how we can roptate it by one place 
+//if we hyave to push the first index at the end then we will need to adjacent swap from the starting so that the first element goes to the loast
+//temp = arr[o ]
+//for(int i =1;i<n;i++)){
+//{ arr[i-1]=arr[i];}
+//}
+//O(N)-space 
