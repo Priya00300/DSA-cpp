624e8706582945d84bc0bd2adf7e2727df865a8a
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-28 13:57:05 +0000

    array

--- Arrays/linearsearch.cpp
+++ Arrays/linearsearch.cpp
@@ -1,1 +1,26 @@
 //taking the nu,mber as variable checking if it exists is existes returning the num if not then then it will return -1
+#include <iostream>
+using namespace std;
+int linearsearch(int arr[],int n,int key){
+  for(int i=0;i<n;i++){
+    if(arr[i]==key){
+      return i;
+    }}
+    return -1;
+  };
+
+int main(){
+  cout<<"enter the size of the array\n";
+  int n;
+  cin>>n;
+  int arr[n];
+  cout<<"enter the elements of the array\n";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  cout<<"enter the element to be searched\n";
+  int key;
+  cin>>key;
+  cout<<linearsearch(arr,n,key);
+  return 0;
+}
