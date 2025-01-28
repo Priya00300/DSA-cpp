1605f8f08b8793f01d3b290aa8f87ec4baae47c1
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-28 19:54:14 +0000

    bs

--- binarysearch/firstandlast.cpp
+++ binarysearch/firstandlast.cpp
@@ -15,22 +15,28 @@
 //}}
 
 #include<iostream>
+#include <vector>
 using namespace std;
-void firstandlast(int arr[],int n,int target){
+void firstandlast(const vector<int>& arr,int n,int target){
   int first= -1;
   int last = -1;
-  if(arr[i]=target){
-    if(first==-1){
-      first =i;
-    }
-    last =1;
-  }
+  for(int i=0;i<n;i++){ 
+    if(arr[i]==target){
+        if(first==-1){
+                 first =i;
+                 }
+           last =i;
+          }}
+
+  cout << "First Occurrence: " << first << endl;
+  cout << "Last Occurrence: " << last << endl;
+
 }
 int main(){
   int n;
   cout<< "enter the size of the array";
   cin >>n;
-  int arr[n];
+  vector<int> arr(n);
   cout<< "enter the elements of the array";
   for (int i =0; i<n;i++){
     cin >> arr[i];
@@ -39,8 +45,5 @@ int main(){
   cout<< "enter the target";
   cin >> target;
   firstandlast(arr,n,target);
-  cout<< "first occurence is at"<<first;
-  cout<< "last occurence is at"<<last;
-  for(int i=0;i<n;i++){
-    cout << arr[i] << " ";}
+  return 0;
 }
