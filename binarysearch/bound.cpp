d7a4660223a01fb23af760dd4311f4531a107671
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-28 17:25:15 +0000

    bs

--- binarysearch/bound.cpp
+++ binarysearch/bound.cpp
@@ -13,7 +13,7 @@ void sort(int arr[], int n){
 for(int i=n-1;i>0;i--){
   int didswap=0;
   for(int j=0;j<i;j++){
-    if(arr[j]>arr[j+1]{
+    if(arr[j]>arr[j+1]){
       temp = arr[j];
       arr[j]=arr[j+1];
       arr[j+1]=temp;
