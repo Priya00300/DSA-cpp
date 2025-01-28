884c1bafbd6f94ac95e83972878604acdfc61fe7
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-28 20:47:54 +0000

    array

--- Arrays/missingnumber.cpp
+++ Arrays/missingnumber.cpp
@@ -4,7 +4,7 @@ int missingnumber(int arr[],int n){
   int sum=0;
   int totalsum=n*(n+1)/2;
   
-  for(i=0;i<n;++){
+  for(int i=0;i<n-1;i++){
     sum+=arr[i];
     
   }
@@ -18,13 +18,13 @@ int main()
   int n;
   cout<<"enter the size of the array";
   cin>>n;
-  int arr[n];
+  int arr[n-1];
   cout<<"enter the elements of the array";
-  for(int i=0;i<n;i++)
+  for(int i=0;i<n-1;i++)
     {
       cin>>arr[i];
     }
-  int missingnumber = missingnumber(arr,n);
-  cout<<"the missing number is"<<missingnumber;
+  int missingnum = missingnumber(arr,n);
+  cout<<"the missing number is"<<missingnum<<endl;
   return 0;
 }
