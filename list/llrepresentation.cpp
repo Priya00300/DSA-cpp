1bdfc26350b3d2a43598240fdf7eab256b13c0bd
Priya00300 <144716779+Priya00300@users.noreply.github.com>
2025-01-30 15:29:13 +0000

    ll

---
+++ list/llrepresentation.cpp
@@ -0,0 +1,34 @@
+//stack and queue data structure
+//in real lif ewe always use it in the browser
+//it is one d it remembers what is the position for the next and not he previous 
+//it is a linear data structure
+//representation-
+//int n =2;
+//int y =&n;
+//cout<<y;
+//here we use &n to define the position or address for the n ewlement ---pointer to n
+// self defined data type
+// struct node{
+//int data;
+ // Node* next;
+//Node(data1,next1) {
+ // data =data1;
+  //next = next1;
+//}
+//Node n = Node(1,nullptr);
+//Node* y = &n;
+
+#include<iostream>
+using namespace std;
+
+struct node{
+int data;
+node* next;
+node(val,next1) : data(val), next(next1) {}
+}
+
+int main(){
+  vector<int> v = [1,2,3,4,5];
+  Noden* head = new Node(arr[0],nullptr);
+  cout<< y;
+}
